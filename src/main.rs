919550bab93baed9103baa78b0987648c3091ca0
wujianyoujian <1536441482@qq.com>
2022-01-26 23:54:17 +0800

    feat: update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // 显示引入io到当前作用域中
 use std::io;
+use rand::Rng;
 
 // main是程序的入口
 fn main() {
@@ -7,6 +8,10 @@ fn main() {
 
     println!("Please input your guess");
 
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is: {}", secret_number);
+    
     // mut 可变修饰，变量默认
     let mut guess = String::new();
 
